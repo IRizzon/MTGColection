313f9c229fcd7347d782b04d376df4e40c62e0fd
Ítalo Rizzon <italomarquesrizzon@gmail.com>
2023-11-24 16:55:49 -0300

    Primeiro Commit
    Inicio do projeto com algumas das telas iniciais, e ideia inicial do projeto implementada

---
+++ MagicColection/app/src/main/java/com/example/magiccolection/View/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.magiccolection.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.magiccolection.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ MagicColection/app/src/main/java/com/example/magiccolection/View/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.example.magiccolection.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.magiccolection.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
