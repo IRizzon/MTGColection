730b6274e9d9bdbd195cc3dc3e59334e9ed615ea
Ítalo Rizzon <italomarquesrizzon@gmail.com>
2023-11-27 16:52:53 -0300

    Splash funcional, Spinner Adicionada

--- MagicColection/app/src/main/java/com/example/magiccolection/View/SplashActivity.java
+++ MagicColection/app/src/main/java/com/example/magiccolection/View/SplashActivity.java
@@ -2,15 +2,36 @@ package com.example.magiccolection.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.example.magiccolection.R;
 
 public class SplashActivity extends AppCompatActivity {
 
+    public static final int TIME_OUT_SPLASH = 3000;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        TelaSplash();
+    }
+
+    private void TelaSplash(){
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent telaLogin = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(telaLogin);
+                finish();
+
+            }
+        }, TIME_OUT_SPLASH);
+
     }
 }
